144eea92efa8f57423bbf7f67aa80ba8a0811cc2
babideer <55578454+babideer@users.noreply.github.com>
2020-11-11 12:21:05 +0100

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,33 @@
+<?php
+$link = mysqli_connect("localhost","root","root","nordlid");
+
+//connection check
+if($link===false){
+    die("ERROR: Could not connect" . mysqli_connect_error());
+}
+
+//escape user inputs 
+$fname = mysqli_real_escape_string($link, $_REQUEST['fname']);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+$phone = mysqli_real_escape_string($link, $_REQUEST['phone']);
+
+//attempt insert query execution
+$sql = "INSERT INTO register (fname, email, phone) VALUES('$fname','$email','$phone')";
+if(mysqli_query($link,$sql)){
+    echo "Tak for din registrering!";
+} else{
+    echo "ERROR: Could not execute $sql. " .mysqli_error($link);
+}
+
+//close connection
+mysqli_close($link);
+?>
+<html>
+    <head>
+
+    </head>
+    <body><div id="center_button">    
+        <button onclick="location.href='index.html'">Tilbage til siden</button>
+    </div>
+</body>
+</html>
